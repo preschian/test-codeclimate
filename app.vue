8514ca1f4d44d99bd01c619fef2e790dde87273f
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-27 13:54:51 +0700

    chore: landing

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <div>hello</div>
   </div>
 </template>
+
+<style>
+* {
+  font-family: monospace;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
