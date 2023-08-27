6e2e63fe3547f044b62f904fc2100211d33d6da8
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-27 14:17:44 +0700

    test: composables

---
+++ components/Case.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="number" min="1" max="12" v-model="nums" />
+    <p>text: {{ first(nums) }}</p>
+    <p>text: {{ second(nums) }}</p>
+  </div>
+</template>
+
+<script setup>
+const nums = ref();
+const { first, second } = useNumber();
+</script>
